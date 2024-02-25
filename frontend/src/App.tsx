b4bd9fbf1f228c3be9c9c5982f8b262009911669
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-25 07:57:47 +0100

    initial commit, set up project: react, tailwind, typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <header className='flex justify-between p-4'>
+        <h1 className="text-2xl font-bold text-blue-700">Mapper</h1>
+        <button className="bg-blue-500 px-6 py-2 text-white rounded font-medium hover:bg-blue-600">Log In</button>
+      </header>
+      <div className="flex justify-center mt-12">
+        <h2 className="text-blue-700 text-4xl font-bold">Remember The Places That Matter</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
